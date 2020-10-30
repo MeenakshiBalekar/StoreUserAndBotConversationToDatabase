54f4910cecd6e5b27b6e9b872b35eef56bc4340a
MeenakshiBalekar <58509122+MeenakshiBalekar@users.noreply.github.com>
2020-10-30 15:53:41 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -17,10 +17,10 @@ namespace Microsoft.BotBuilderSamples
     public class Startup
     {
 
-        private const string CosmosServiceEndpoint = "https://cosmodbnew.documents.azure.com:443/";
-        private const string CosmosDBKey = "IY9E6wBnqqLx7tW1JP89Z767EVbN7SPnbNtp5O3GFEMpXPNLC9pMb2f2NLPUcBzGj95D1dhCA3HJ0As7hOJTrg==";
-        private const string CosmosDBDatabaseName = "bot-cosmos-sql-db";
-        private const string CosmosDBCollectionName = "bot-storage";
+        private const string CosmosServiceEndpoint = "";
+        private const string CosmosDBKey = "";
+        private const string CosmosDBDatabaseName = "";
+        private const string CosmosDBCollectionName = "";
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
